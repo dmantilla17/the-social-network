28c3b36858c35ef7d5d27971793014c51a87f2f1
dmantilla17 <derekaraujo11@gmail.com>
2023-04-03 20:50:26 -0400

    reaction js98%

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { builtinModules } = require("module");
+const { model, Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxcharacter: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+module.exports = reactionSchema;
